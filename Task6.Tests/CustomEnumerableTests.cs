66bfccb28eecaacc19911b447c74ea417819401c
Sergey <serik.privet@gmail.com>
2018-04-23 12:00:50 +0300

    Sixth task

--- Task6.Tests/CustomEnumerableTests.cs
+++ Task6.Tests/CustomEnumerableTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Task6.Solution;
 
 namespace Task6.Tests
 {
@@ -16,7 +17,7 @@ namespace Task6.Tests
         {
             int[] expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
 
-            Assert.Inconclusive();
+            CollectionAssert.AreEqual(expected, Generator<int>.GenerateSequence(10, 1, 1, (x1, x2) => x1 + x2).ToArray());
         }
 
         [Test]
@@ -24,15 +25,19 @@ namespace Task6.Tests
         {
             int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
 
-            Assert.Inconclusive();
+            CollectionAssert.AreEqual(expected, Generator<int>.GenerateSequence(10, 0, 1, (x1, x2) => (int)Math.Pow(x1,2)).ToArray());
         }
 
         [Test]
         public void Generator_ForSequence3()
         {
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
+            double[] actual = Generator<double>.GenerateSequence(10, 1, 2, (x1, x2) => x2 + (x1 / x2)).ToArray();
 
-            Assert.Inconclusive();
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i], 0.0000000000001);
+            }
         }
     }
 }
---
+++ Test6.Solution/Generator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task6.Solution
+{
+    public static class Generator<T>
+    {
+        public static IEnumerable<T> GenerateSequence(int size, T first, T second, Func<T, T, T> law)
+        {
+            if (size < 2)
+            {
+                throw new ArgumentException(nameof(size));
+            }
+            
+            yield return first;
+            yield return second;
+            T buffer = first;
+            
+            for (int i = 2; i < size; i++)
+            {
+                buffer = second;
+                yield return second = law(first, buffer);
+                first = buffer;
+            }
+        }
+    }
+}
