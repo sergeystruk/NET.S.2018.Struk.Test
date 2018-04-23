33a4abbafab909cb2c1ce914e6be12b022f2cde0
Sergey <serik.privet@gmail.com>
2018-04-23 12:03:02 +0300

    Sixth task

--- Task6.Tests/CustomEnumerableTests.cs
+++ Task6.Tests/CustomEnumerableTests.cs
@@ -23,21 +23,13 @@ namespace Task6.Tests
         [Test]
         public void Generator_ForSequence2()
         {
-            int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
 
-            CollectionAssert.AreEqual(expected, Generator<int>.GenerateSequence(10, 0, 1, (x1, x2) => (int)Math.Pow(x1,2)).ToArray());
         }
 
         [Test]
         public void Generator_ForSequence3()
         {
-            double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
-            double[] actual = Generator<double>.GenerateSequence(10, 1, 2, (x1, x2) => x2 + (x1 / x2)).ToArray();
 
-            for (int i = 0; i < actual.Length; i++)
-            {
-                Assert.AreEqual(expected[i], actual[i], 0.0000000000001);
-            }
         }
     }
 }
